fa8cf5ce107e31f039a5060226efdd99469ee56b
exclud <shammahnjomo12@gmail.com>
2023-08-09 13:43:32 +0300

    Added the BaseModel Class with the String Representation function

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+
+"""Class Base Model"""
+
+import models
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+class BaseModel:
+    
+    def __str__(self):
+        """String Representation of the BaseModel Class
+
+        Returns:
+            str: Prints [<class name>] (<self.id>) <self.__dict__>
+        """
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
