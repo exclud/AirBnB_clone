2712b51fa26ec23e2e1f8c84ea098ef870787cde
exclud <shammahnjomo12@gmail.com>
2023-08-11 13:39:23 +0300

    Wrote the City and State Classses and initialized them

--- models/city.py
+++ models/city.py
@@ -1,3 +1,19 @@
 #!/usr/bin/python3
+"""Class City which inherits from BaseModel"""
 import models
 from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Class City which inherits from the BaseModel Class
+
+    Args:
+        BaseModel (_type_): _description_
+    """
+    state_id = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of the city class
+        """
+        super().__init__(*args, **kwargs)
--- models/state.py
+++ models/state.py
@@ -3,3 +3,16 @@
 
 import models
 from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """The class State which inherits from BaseModel class
+
+    Args:
+        BaseModel (_type_): The main class which is being inherited from
+    """
+    name = ""
+    
+    def __init__(self, *args, **kwargs):
+        """initialization of the State Class"""
+        super().__init__(*args, **kwargs)
