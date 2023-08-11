3e0e761cecd4ddd2778068f27abd352bf6c4fe2b
exclud <shammahnjomo12@gmail.com>
2023-08-11 13:45:25 +0300

    Wrote the place and review classes successfully

--- models/place.py
+++ models/place.py
@@ -3,3 +3,22 @@
 
 import models
 from models.base_model import BaseModel
+
+
+class Place(BaseModel):
+    """Class Place that inherits from BaseModel"""
+    city_id = ""
+    user_id = ""
+    name = ""
+    description = ""
+    number_rooms = 0
+    number_bathrooms = 0
+    max_guest = 0
+    price_by_night = 0
+    latitude = 0.0
+    longitude = 0.0
+    amenity_ids = []
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of Place attributes"""
+        super().__init__(*args, **kwargs)
--- models/review.py
+++ models/review.py
@@ -3,3 +3,14 @@
 
 import models
 from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Review class that inherits from BaseModel"""
+    place_id = ""
+    user_id = ""
+    text = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialization of Review attributes"""
+        super().__init__(*args, **kwargs)
